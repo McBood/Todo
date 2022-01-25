05450758960a3484dba0199acacae36a349ecdcc
Eugene Sukhovarov <mcboodvk@gmail.com>
2022-01-25 20:45:34 +0300

    add add-form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react"
+import AddButtonList from "./components/AddButtonList";
 import List from "./components/List/List";
 
+import DB from "./assets/db.json"
+
 function App() {
-  return <div className="todo ">
+  return (
+  <div className="todo ">
     <div className="todo__sidebar">
       <List items={[{
         icon: (<svg width="18"
@@ -33,22 +37,10 @@ function App() {
       ]}
       isRemovable
       />
-      <List items={[
-        { 
-          className: "list__add-button",
-          icon: (<svg width="12" 
-                      height="12" 
-                      viewBox="0 0 12 12" 
-                      fill="none" 
-                      xmlns="http://www.w3.org/2000/svg">
-                <path d="M6 1V11" stroke="#868686" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                <path d="M1 6H11" stroke="#868686" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                </svg>),
-          name: "Добавить список",
-        }
-      ]}/>
+    <AddButtonList colors={DB.colors}/>
     </div>
   </div>
+  )
   }
 
 export default App;
